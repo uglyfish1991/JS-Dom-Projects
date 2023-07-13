bd2de5339c8baf1f507d54bd39ce995c62116a0e
Katy Long <katy.long@wearecodenation.com>
2023-07-13 14:34:57 +0100

    setInterval added

--- drumKit/script.js
+++ drumKit/script.js
@@ -1,17 +1,17 @@
 //----------------------------------Variables-----------------------------------------//
 
 const buttons = document.querySelector('.buttons');
-
+const volume = document.getElementById("volume")
 const sounds = {
-  A: new Audio('sounds/boom.wav'),
-  S: new Audio('sounds/clap.wav'),
-  D: new Audio('sounds/hihat.wav'),
-  F: new Audio('sounds/kick.wav'),
-  G: new Audio('sounds/openhat.wav'),
-  H: new Audio('sounds/ride.wav'),
-  J: new Audio('sounds/snare.wav'),
-  K: new Audio('sounds/tink.wav'),
-  L: new Audio('sounds/tom.wav'),
+  a: new Audio('sounds/boom.wav'),
+  s: new Audio('sounds/clap.wav'),
+  d: new Audio('sounds/hihat.wav'),
+  f: new Audio('sounds/kick.wav'),
+  g: new Audio('sounds/openhat.wav'),
+  h: new Audio('sounds/ride.wav'),
+  j: new Audio('sounds/snare.wav'),
+  k: new Audio('sounds/tink.wav'),
+  l: new Audio('sounds/tom.wav'),
 }
 
 //----------------------------------Functions-----------------------------------------//
@@ -19,25 +19,40 @@ const sounds = {
 const playDrum = (key)=>{
   if (key in sounds){
     sounds[key].play();
+    move()
   }
 }
 
 const hightlightButton = (key)=>{
-    if (key){
+    if (key in sounds){
         let div = document.getElementById(key)
-        div.classList.add('clicked')
+        div.classList.add('clicked');
 
         setTimeout(()=>{
-            div.classList.remove('clicked')
+            div.classList.remove('clicked');
         }, 400)
     }
-}
+  }
+
+function move(){
+const myInterval = setInterval(raiseVolume, 15)
+let value = 0
+function raiseVolume(){
+  console.log(volume.value);
+  if (value == "14"){
+    clearInterval(myInterval);
+    volume.value = "0";
+  } else {
+    value++;
+    volume.value = value;
+  }
+}}
 
 //----------------------------------Event Listeners-----------------------------------------//
 
 document.addEventListener('keydown', (event)=>{
-  playDrum(event.key.toUpperCase());
-  hightlightButton(event.key.toUpperCase());
+  playDrum(event.key.toLowerCase());
+  hightlightButton(event.key.toLowerCase());
 });
 
 buttons.addEventListener('click',(event)=>{
